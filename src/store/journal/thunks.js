0006e2660a08250d39af31133fbb0fe79a7d07a5
Lucio Berté <104603180+LucioABerte@users.noreply.github.com>
2023-02-23 17:20:26 -0300

    Update thunks.js

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,7 +1,14 @@
-export const startNewNote = () => {
-    return async(dispatch) => {
+import {collection, doc, setDoc} from "firebase/firestore/lite"
+import { FirebaseDB } from "../../firebase/config"
+import { loadNotes } from "../../helpers/loadNotes"
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./journalSlice"
 
-        // uid
+export const startNewNote = () => {
+    return async(dispatch, getState) => {
+
+        dispatch( savingNewNote())
+        
+        const {uid} = getState().auth
 
         const newNote = {
             title: "",
@@ -9,12 +16,24 @@ export const startNewNote = () => {
             date: new Date().getTime(),
         }
 
+        const newDoc = doc( collection( FirebaseDB, `${uid}/journal/notes` ))
+        await setDoc( newDoc, newNote)
 
-        // dispatch
+        newNote.id = newDoc.id
 
-        // dispatch ( newNote )
+        dispatch(addNewEmptyNote(newNote))
+        dispatch( setActiveNote(newNote))
 
-        // dispatch ( activarNote )
+    }
+}
 
+export const startLoadingNotes = () => {
+    return async(dispatch, getState) => {
+        
+        const {uid} = getState().auth
+        if(!uid) throw new Error("El UID del usuario no existe")
+
+        const notes = await loadNotes(uid)
+        dispatch(setNotes(notes))
     }
 }
