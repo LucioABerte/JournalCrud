2baf41ea3480bae46e247b7736f2e2db8e234466
Lucio Berté <104603180+LucioABerte@users.noreply.github.com>
2023-02-23 17:16:56 -0300

    Update SideBar.jsx

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,12 @@
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
-import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
+import {SideBarItem} from './';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
   
     const {displayName} = useSelector(state => state.auth)
+    
+    const {notes} = useSelector(state => state.journal)
   
   
     return (
@@ -29,18 +31,8 @@ export const SideBar = ({ drawerWidth = 240 }) => {
 
             <List>
                 {
-                    ['Enero','Febrero','Marzo','Abril'].map( text => (
-                        <ListItem key={ text } disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={ text } />
-                                    <ListItemText secondary={ 'Exercitation cillum irure elit consectetur.' } />
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
+                    notes.map( note => (
+                        <SideBarItem key={note.id} { ...note } />
                     ))
                 }
             </List>
