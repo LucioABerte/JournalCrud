b653e375cab2da294e29e177dfd0beb3e58a5dd4
Lucio Berté <104603180+LucioABerte@users.noreply.github.com>
2023-02-23 17:15:20 -0300

    Update useCheckAuth.js

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FirebaseAuth } from '../firebase/config'
 import { login, logout } from '../store/auth/authSlice'
+import { startLoadingNotes } from '../store/journal/thunks'
 
 export const useCheckAuth = () => {
     const {status} = useSelector(state => state.auth)
@@ -15,6 +16,7 @@ export const useCheckAuth = () => {
         
         const {uid, email, displayName, photoURL} = user 
         dispatch( login({uid, email, displayName, photoURL}))
+        dispatch(startLoadingNotes())
       })
       
     }, [])
