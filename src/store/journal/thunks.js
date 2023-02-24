169f4c91e7de3a731de7ac7fe7dd1fcc93f8dd6d
Lucio Berté <104603180+LucioABerte@users.noreply.github.com>
2023-02-24 18:56:04 -0300

    Update thunks.js

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,7 +1,7 @@
 import {collection, doc, setDoc} from "firebase/firestore/lite"
 import { FirebaseDB } from "../../firebase/config"
 import { loadNotes } from "../../helpers/loadNotes"
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./journalSlice"
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setSaving, updateNote } from "./journalSlice"
 
 export const startNewNote = () => {
     return async(dispatch, getState) => {
@@ -35,5 +35,23 @@ export const startLoadingNotes = () => {
 
         const notes = await loadNotes(uid)
         dispatch(setNotes(notes))
+    }
+}
+
+export const startSaveNote = () => {
+    return async(dispatch, getState) => {
+
+        dispatch( setSaving())
+
+        const {uid} = getState().auth
+        const {active:note} = getState().journal
+
+        const noteToFirestore = {...note}
+        delete noteToFirestore.id
+
+        const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`)
+        await setDoc(docRef, noteToFirestore, { merge: true})
+
+        dispatch(updateNote(note))
     }
 }
